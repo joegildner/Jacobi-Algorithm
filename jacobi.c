243459aa1693253f13a3474bf47afe5d39c39a91
Joe Gildner <joegildner0@gmail.com>
2018-12-03 11:25:36 -0800

    Working for one iteration of the jacobi algorithm, with as many threads as specified.

--- jacobi.c
+++ jacobi.c
@@ -9,19 +9,31 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#define THREAD_COUNT 96
+#define THREAD_COUNT 13
 
 pthread_t thread_ids[THREAD_COUNT];
+int thread_nums[THREAD_COUNT];
 
-int results[THREAD_COUNT];
+double current_matrix[1024][1024];
+double updated_matrix[1024][1024];
 
-void* mathstuff(void* args);
+void* jacobi_algorithm(void* args);
 
 int main(int argc, char* argv[]){
+  FILE* inputmatrix = fopen("./input.mtx","r");
 
-  for(int i=0; i<THREAD_COUNT; i++){
-    results[i] = 0;
-    if(pthread_create(&(thread_ids[i]), NULL, &mathstuff,NULL) != 0){
+  for(int row=0; row<1024; row++){
+    for(int col=0; col<1024; col++){
+      fscanf(inputmatrix, "%lf ", &(current_matrix[row][col]));
+      updated_matrix[row][col] = -1.0;
+    }
+  }
+
+
+  
+  for(int i=0; i<THREAD_COUNT; ++i){
+    thread_nums[i] = i;
+    if(pthread_create(&(thread_ids[i]), NULL, &jacobi_algorithm, (void*)(&thread_nums[i])) != 0){
       perror("pthread");
     }else{
       printf("Created thread %d successfully\n",i);
@@ -32,21 +44,28 @@ int main(int argc, char* argv[]){
     pthread_join(thread_ids[i],NULL);
   }
 
-  for(int i=0; i<THREAD_COUNT; i++){
-    printf("%d\n",results[i]);
+  for(int row = 1; row<1023; row++){
+    for(int col = 1; col<1023; col++){
+      if(updated_matrix[row][col] == -1.0)
+        printf("ERROR %d,%d\n", row, col);
+    }
   }
+
 
 }
 
-void* mathstuff(void* args){
-  pthread_t thisthread = pthread_self();
+void* jacobi_algorithm(void* args){
+  int thd = *((int*) args);
 
-  printf("%ld",thisthread);
-  // for(int i=0; i<THREAD_COUNT; i++){
-  //   if(pthread_equal(thisthread,thread_ids[i])){
-  //     results[i] = i;
-  //   }
-  // }
+  for(int row = 1; row<1023; row++){
+    for(int col = thd+1; col<1023; col += THREAD_COUNT){
+      double above = current_matrix[row-1][col];
+      double below = current_matrix[row+1][col];
+      double left = current_matrix[row][col-1];
+      double right = current_matrix[row][col+1];
+      updated_matrix[row][col] = (above + below + left +right)/4;
+    }
+  }
 
-  return NULL;
+  return NULL; 
 }
