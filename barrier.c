8b20e1c7e6b09d9e064bff7879b1b9fb6a0e9ae2
Muakong Joey Xiong <xiongm@LabImage2016-17>
2018-12-03 16:30:15 -0800

    fixed all warnings in the program but have yet to test if the barriers will work

---
+++ barrier.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <pthread.h>
+typedef struct barrier{
+  int thread_count;
+  pthread_mutex_t* m;
+  pthread_cond_t allHere;
+  pthread_cond_t allGone;
+  int total;
+  int here;
+  int leaving;
+}barrier;
+int main()
+{
+  return 0;
+}
+barrier* barrier_new(int count)
+{
+  barrier* b = malloc(sizeof(barrier));
+  if(b!=NULL)
+  {
+    pthread_mutex_init(b->m,NULL);
+    pthread_cond_init(&(b->allHere),NULL);
+    pthread_cond_init(&(b->allGone),NULL);
+    b->total = 0;
+    b->here = 0;
+    b->leaving = 0;
+  }
+  return b;
+}
+void barrier_enter(barrier* b, int thd)
+{
+  pthread_mutex_lock(b->m);
+  while(b->leaving)
+  {
+    pthread_cond_wait(&(b->allGone),b->m);
+    pthread_cond_signal(&b->allGone);
+  }
+  b->here++;
+  while(b->here != b->total && b->here != b->leaving)
+  {
+    pthread_cond_wait(&(b->allHere), b->m);
+  }
+  b->leaving = 1;
+  pthread_cond_signal(&(b->allHere));
+  b->here--;
+  if(b->here == 0)
+  {
+    b->leaving = 0;
+    pthread_cond_signal(&(b->allGone));
+  }
+  pthread_mutex_unlock(b->m);
+}
